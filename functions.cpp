6eebbd83d9d45f00d7184ccd0a6e2bfb3c854180
arsheenkour <arsheenkour47@gmail.com>
2024-06-11 11:33:26 +0530

    first commit

---
+++ functions.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+/*void printstuff()
+{
+	cout<<"writing my first function"
+}*/
+int sum(int a, int b)
+{
+	int c;
+	c=a+b;
+	return c;
+}
+int main()
+{
+	//printstuff();
+	int a,b;
+	int add_sum;
+	a=3;
+	b=5;
+	add_sum=sum(a,b);
+	cout<<add_sum;
+}
